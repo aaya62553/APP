1ec48713bbd5a1e9e22f69659606b96f8ecdee4e
Noir2002 <144689628+Noir2002@users.noreply.github.com>
2024-11-15 16:46:38 +0100

    Add files via upload

--- myAutocorrelation.m
+++ myAutocorrelation.m
@@ -8,23 +8,22 @@
 %       tx  : time vector 
 % -------------------------------------------------------------------------
 function [Cxx,tx] = myAutocorrelation(x,Fe,D)
-
-kmax = round(D*Fe);
-Nx   = length(x);
-
-if Nx<2*kmax
-    fprintf('ERROR : duration of x < 2*D !')
-    Cxx = [];
-    tx  = [];
-else
-    x0   = x(1:kmax);
-    Cxx  = zeros(1,kmax);
-    for k = 0:kmax-1
-        xs=[x(k,Nx-k) x(1,k-1)];
-           
-        Cxx(k+1) = mean(x0.*xs);
+    kmax = round(D*Fe);
+    Nx   = length(x);
+    if Nx<2*kmax
+        fprintf('ERROR : duration of x < 2*D !')
+        Cxx = [];
+        tx  = [];
+    else
+        x0   = x(1:kmax);
+        Cxx  = zeros(1,kmax);
+        for k = 0:kmax-1
+            
+            xs       = x(k+1:min(kmax+k, Nx));   
+            Cxx(k+1) = mean(x0.*xs);
+        end
+        tx = (0:kmax-1)/Fe;
     end
-    tx = (0:kmax)/Fe;
 end
 
 
