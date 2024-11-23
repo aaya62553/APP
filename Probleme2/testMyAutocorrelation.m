8cc00c277275996c7e63df67b79bb2a5a14a1819
aquio600 <lavanantclement23@gmail.com>
2024-11-23 12:51:43 +0100

    Add files via upload

--- Probleme2/testMyAutocorrelation.m
+++ Probleme2/testMyAutocorrelation.m
@@ -16,7 +16,7 @@ clc
 Fe  = 10e4;             % fréquence d'echantillonage (Hz)
 Te = 1/Fe;
 
-f0  = 500;             %  fréquence des signaux  (Hz)
+f0  = 700;             %  fréquence des signaux  (Hz)
 D   = 10/f0;             % durée ( 10 périodes)
 
 % ---- Signal generation
@@ -27,7 +27,8 @@ N = length(t);
 x = cos(2*pi*f0*t);   %cosinus de fréquence 500hz
 y = sin(2*pi*f0*t) ;    %sinus de fréquence 500hz
 f=t ;   %fonction linéaire
-
+[g,Fe]= audioread("FluteNote01.mp3");
+    %u=0:lenght(g)
 % ---- affichage de tests (2 par fenêtre le signal et son autocorrelation)
 figure('Name','Autocorrelation') %affichage du sinus
 subplot(2,1,1);
@@ -58,20 +59,44 @@ grid on
 title 'Autocorrelation of signal x '
 xlabel 's'
 ylabel 'W'
-
-figure('Name','Autocorrelation')%affichage de la fonction linéaire
-subplot(2,1,1);
-plot(t,f);
-grid on;
-title 'Signal f - time domain'
-xlabel 's'
-ylabel 'V'
-[Cff,tf] = AutoCorelation(f,Fe);
-subplot(2,1,2);
-plot(tf,Cff);%affichage de sa correlation
-grid on
-title 'Autocorrelation of signal x '
-xlabel 's'
-ylabel 'W'
+%identification de la période
+N=length(Cxx);
+for i = 2:N ;
+    if Cxx(i)>= Cxx(i-1) && Cxx(i) >= Cxx(i+1);
+        "la période est de "+i*Te+"s"
+        break
+    end
 
 
+end
+% 
+% figure('Name','Autocorrelation')%affichage de la fonction linéaire
+% subplot(2,1,1);
+% plot(t,f);
+% grid on;
+% title 'Signal f - time domain'
+% xlabel 's'
+% ylabel 'V'
+% [Cff,tf] = AutoCorelation(f,Fe);
+% subplot(2,1,2);
+% plot(tf,Cff);%affichage de sa correlation
+% grid on
+% title 'Autocorrelation of signal x '
+% xlabel 's'
+% ylabel 'W'
+% 
+% figure('Name','Autocorrelation')%affichage du signal audio
+% subplot(2,1,1);
+% plot(g);
+% grid on;
+% title 'Signal g - time domain'
+% xlabel 's'
+% ylabel 'V'
+% [Cgg,tg] = AutoCorelation(g',Fe);
+% subplot(2,1,2);
+% %plot(tg,Cgg);%affichage de sa correlation
+% grid on
+% title 'Autocorrelation of signal g '
+% xlabel 's'
+% ylabel 'W'
+% 
