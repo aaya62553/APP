80cf12f90545da9b3ea6909361d463183081dbc8
Gaut2 <gautier2.leclair@gmail.com>
2024-11-23 18:46:44 +0100

    Update TestDetectionF0Freq.m

--- Probleme2/TestDetectionF0Freq.m
+++ Probleme2/TestDetectionF0Freq.m
@@ -14,6 +14,7 @@ close all;
 clc;
 
 %Paramètre du signal :
+
 Fe = 10e4;
 Te = 1/Fe;
 
@@ -25,76 +26,52 @@ N = length(t);
 
 x = cos (2 * pi *f0 * t);
 
-%graphique test 1
-
-figure('Name','DetectionF0Freq')%affichage du cosinus
-subplot(3,1,1);
-plot(t,x);
-grid on;
-title 'test pour f0 = 500 Hz'
-xlabel 's'
-ylabel 'V'
+% test 1 pour f0 = 500Hz
 
 [f0_detectee] = DetectionF0Freq(x,Fe,true);
 
+disp ('Test pour un signal de fréquence de 500 Hz : ');
 disp(['Fréquence fondamentale détectée : ', num2str(f0_detectee), ' Hz']);
 
 
-%graphique test 2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 f1 = 200;
-
-
-
 x2 = sin ( 2 * pi * f1 * t);
 
-figure('Name','DetectionF0Freq') %affichage du cosinus
-subplot(3,1,2);
-plot(t,x2);
-grid on;
-title 'Signal x - time domain'
-xlabel 's'
-ylabel 'V'
+% test 2 pour f1 = 200Hz
 
 [f1_detectee] = DetectionF0Freq(x2,Fe,true);
 
+disp ('Test pour un signal de fréquence de 200 Hz : ');
 disp(['Fréquence fondamentale détectée : ', num2str(f1_detectee), ' Hz']);
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%graphique test 3
+
 f2 = 600;
-
 x3 = sin ( 2 * pi * f2 * t);
 
-figure('Name','DetectionF0Freq') %affichage du cosinus
-subplot(3,1,3);
-plot(t,x3);
-grid on;
-title 'Signal x - time domain'
-xlabel 's'
-ylabel 'V'
+% test 3 pour f2 = 600Hz
 
 [f2_detectee] = DetectionF0Freq(x3,Fe,true);
 
+disp ('Test pour un signal de fréquence de 600 Hz : ');
 disp(['Fréquence fondamentale détectée : ', num2str(f2_detectee), ' Hz']);
 
 
-%graphique test 4
-f3 = 1110;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+f3 = 1110;
 x4 = cos ( 2 * pi * f3 * t);
 
-figure('Name','DetectionF0Freq') %affichage du cosinus
-subplot(3,1,3);
-plot(t,x3);
-grid on;
-title 'Signal x - time domain'
-xlabel 's'
-ylabel 'V'
+% test 4 pour f3 = 1110 Hz
 
 [f3_detectee] = DetectionF0Freq(x4,Fe,true);
 
+disp ('Test pour un signal de fréquence de 1110 Hz : ');
 disp(['Fréquence fondamentale détectée : ', num2str(f3_detectee), ' Hz']);
 
 
