5a2aa56341e4cd3ddbd376ad9647aa494efbda68
YAZDI Aaron <130841526+aaya62553@users.noreply.github.com>
2024-11-24 12:33:15 +0100

    Update DetectNoteTestV2.m

--- Probleme2/DetectNoteTestV2.m
+++ Probleme2/DetectNoteTestV2.m
@@ -1,3 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Fichier test de DetectNote
+% -------------------------------------------------------------------------
+% Description : 
+% ce fichier sert pour tester et afficher les résultats du programme
+% DetectNote (NE FONCTIONNE PAS SANS AVOIR OUVERT DetectNote)
+%
+% Auteur : G10E
+% -------------------------------------------------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Définir un vecteur de fréquences à tester
 frequencies = [440, 466.16, 493.88, NaN, 261.63]; % La4, La#4, Si4, NaN, Do4
 
