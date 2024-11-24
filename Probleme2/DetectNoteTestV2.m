f879aa01f7fad27c57d37ff6d65f85244773d696
YAZDI Aaron <130841526+aaya62553@users.noreply.github.com>
2024-11-24 12:32:24 +0100

    Update DetectNoteTestV2.m

--- Probleme2/DetectNoteTestV2.m
+++ Probleme2/DetectNoteTestV2.m
@@ -2,7 +2,7 @@
 frequencies = [440, 466.16, 493.88, NaN, 261.63]; % La4, La#4, Si4, NaN, Do4
 
 % Appeler la fonction DetectNote
-SegmentNoteNames = DetectNoteE(frequencies);
+SegmentNoteNames = DetectNote(frequencies);
 
 % Afficher les résultats
 disp('Fréquences :');
